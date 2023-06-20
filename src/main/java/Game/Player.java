3abfc0acedf61a3f30964f5bd6833723ae629fd2
ShaneGons <129097971+ShaneGons@users.noreply.github.com>
2023-06-20 16:12:50 +0100

    not needed

--- src/main/java/Game/Player.java
+++
@@ -1,7 +0,0 @@
-package Game;
-public class Player {
-    private String userName;
-    private char colour;
-    private int points;
-    private int rating;
-}
--- src/main/java/libraryFunctions/iPrintable.java
+++
@@ -1,9 +0,0 @@
-
-package libraryFunctions;
-
-
-public interface iPrintable {
-    //Gets the fields which are printable from the objects
-    //Format of printable fields should be "Forename: Value, Surname: Value, ... "
-    String getPrintableString();
-}
